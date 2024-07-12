32087c52028cf0ca939c2403533f7e37056c8c54
ALagu Aravind <aalagu748@gmail.com>
2024-07-12 07:21:31 +0530

    update

--- src/components/playlist.jsx
+++ src/components/playlist.jsx
@@ -37,7 +37,7 @@ function Playlist() {
   }
 
   const fetchPlaylistData = useCallback(async () => {
-    setIsLoading(true);
+   
     try {
       const response = await fetch(`https://cineback-0zol.onrender.com/playlist/all/${userId}`);
       if (response.ok) {
@@ -122,6 +122,7 @@ function Playlist() {
             movies: [],
           }),
         });
+        
 
         if (response.ok) {
           await fetchUserPlaylists();
